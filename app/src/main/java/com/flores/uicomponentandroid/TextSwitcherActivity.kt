684fd1cd767751d44fbe6555b040c8fd5f373b24
Bill Flores <bilizen3@gmail.com>
2020-03-11 00:17:20 -0500

    add functionality to TextSwitcher on module app

--- app/src/main/java/com/flores/uicomponentandroid/TextSwitcherActivity.kt
+++ app/src/main/java/com/flores/uicomponentandroid/TextSwitcherActivity.kt
@@ -1,7 +1,13 @@
 package com.flores.uicomponentandroid
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import android.widget.ViewSwitcher
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_text_switcher.*
+
 
 /**
  * TextSwitcherActivity
@@ -14,5 +20,27 @@ class TextSwitcherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_switcher)
+        initUi()
     }
+
+    private fun initUi() {
+        textSwitcher_ts.setFactory(ViewSwitcher.ViewFactory {
+            val textView = TextView(this);
+            textView.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+            return@ViewFactory textView
+        })
+
+        textSwitcher_ts.inAnimation =
+            AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left)
+        textSwitcher_ts.outAnimation = AnimationUtils.loadAnimation(
+            this, android.R.anim
+                .slide_out_right
+        )
+        textSwitcher_ts.setCurrentText("Buenos dias");
+        textSwitcher_btn_start.setOnClickListener {
+            textSwitcher_ts.setCurrentText("Buenas noches")
+        }
+    }
+
+
 }
