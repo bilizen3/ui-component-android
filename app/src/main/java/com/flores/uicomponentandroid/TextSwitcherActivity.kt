4e6a11bebfad93d15f9e8bc1a79a43b76c7ccaff
Bill Flores <bilizen3@gmail.com>
2020-03-10 23:42:03 -0500

    add activity text switcher to module app

---
+++ app/src/main/java/com/flores/uicomponentandroid/TextSwitcherActivity.kt
@@ -0,0 +1,18 @@
+package com.flores.uicomponentandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * TextSwitcherActivity
+ *
+ * @author Bill Flores - bilizen3@gmail.com
+ * @since 03/10/2020
+ */
+class TextSwitcherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_switcher)
+    }
+}
